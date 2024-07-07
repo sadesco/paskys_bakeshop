f6fc0f98086830b684effc50cef11c4573725bd5
sadesco <147342700+sadesco@users.noreply.github.com>
2024-07-07 15:42:37 +0800

    first commit

---
+++ backend/base/products.py
@@ -0,0 +1,53 @@
+products = [
+    {
+      '_id': '1',
+      'name': 'Mooncakes',
+      'image': '/images/mooncakes.png',
+      'description':
+        'Mooncakes are traditional Chinese pastries with a thin, tender crust and a rich, dense filling, offering flavors such as ube, red bean, and lotus with salted egg yolk and various nuts and seeds.',
+      'category': 'Dessert',
+      'price': 700.00,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Pineapple Cakes',
+      'image': '/images/pineapple.png',
+      'description':
+        'Pineapple cakes are small, traditional Taiwanese pastries with a buttery, tender crust and a sweet, tangy pineapple filling, offering a delightful balance of flavors in each bite-sized treat.',
+      'category': 'Dessert',
+      'price': 599.99,
+      'countInStock': 0,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Frozen Mochi Mooncakes',
+      'image': '/images/mochi.png',
+      'description':
+        'Frozen mochi mooncakes are vibrant, colorful treats with a chewy, glutinous rice exterior and a luscious, ice cream-like filling, offering a refreshing and delightful twist on traditional mooncakes.',
+      'category': 'Dessert',
+      'price': 899.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'Pasky\'s Soft Baked Bread',
+      'image': '/images/bread.png',
+      'description':
+        'Pasky\'s Soft Baked Bread is a delectable Asian style bread, known for its incredibly fluffy and airy texture, with a subtle sweetness that makes each bite a comforting and delightful experience.',
+      'category': 'Baked Good',
+      'price': 399.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+
+  ]
+  
+  
