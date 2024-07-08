5992e483b9ec74618b52544cacec75ff704f6f65
sadesco <147342700+sadesco@users.noreply.github.com>
2024-07-08 14:48:59 +0800

    frontend stuff

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+import Product from '../components/Product'
+
+
+
+function HomeScreen() {
+  const [products, setProducts] = useStae([])
+
+  useEffect(( => {
+      async function fetchProducts(){
+        const{ data } = await axios.get('http://127.0.0.1:8000/api/products/')
+        setProducts(data)
+      }
+
+      fetchProducts()
+  }))
+
+  return (
+    <div>
+      <h1>Baked Goods</h1>
+      <Row>
+        {products.map(product => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
