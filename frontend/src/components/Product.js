87cd4461eec7ec9ede001ae0d3119af520ad490e
sadesco <147342700+sadesco@users.noreply.github.com>
2024-08-08 01:01:08 -0400

    Product.js new

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -29,7 +29,7 @@ export default Product
 function Product({ product }) {
   return (
     <Card className='my-3 p-3 rounded'>
-      <Link to={`/product/${product._id}`}>
+      <Link to={`/product/{product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
 
